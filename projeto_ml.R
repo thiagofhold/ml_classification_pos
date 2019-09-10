5e1ccf91972661638b197eb1fa2e417f4dd9e2d4
Komando Shiloh Laris <shilohlarris@Komandos-MacBook-Pro.local>
2019-09-10 19:39:47 -0300

    formatando dataset

--- projeto_ml.R
+++ projeto_ml.R
@@ -4,11 +4,35 @@
 ## FMU 
 
 #######################################################################
+install.packages("tidyverse")
+installed.packages("dplyr")
+library(tidyverse)
+library(dplyr)
 
+##setting work directory
 setwd("~/Desktop/projeto_fred/ml_classification_pos")
+
+##importando dataset e atribuindo numa variavel
 movie <- read.csv("movie_metadata.csv",sep = ",")
 
+##Deletando os campos de NA (vazios)
 movie <- na.omit(movie)
 str(movie)
 
-new_movies <-filter(movie, title_year <=2000)
+##filtrando os dados para trabalhar so com filmes depois de 2006
+new_movies <- filter(movie, title_year >= 2000)
+str(new_movies)
+##duration, genres, plot_keywords, language, country, budget
+colnames(new_movies)
+ 
+str(new_movies)
+
+##Transformando as colunas de duração e budget em fator
+new_movies$duration <- as.factor(new_movies$duration)
+new_movies$budget <- as.factor(new_movies$budget)
+
+str(new_movies)
+
+new_movies$movie_facebook_likes
+new_movies$budget
+
