f3974bb102a22d652417e6d789a9aef7ab78947e
shilohlarris <shilohlarris@gmail.com>
2019-09-18 21:08:40 -0300

    tratamento dos dados melhorado

--- projeto_ml.R
+++ projeto_ml.R
@@ -13,15 +13,12 @@ library(dplyr)
 setwd("~/Desktop/projeto_fred/ml_classification_pos")
 
 ##importando dataset e atribuindo numa variavel
-movie <- read.csv("movie_metadata.csv",sep = ",")
+new_movies <- read.csv("movie_metadata.csv",sep = ",")
 
 ##Deletando os campos de NA (vazios)
-movie <- na.omit(movie)
-str(movie)
-
-##filtrando os dados para trabalhar so com filmes depois de 2006
-new_movies <- filter(movie, title_year >= 2000)
+new_movies <- na.omit(new_movies)
 str(new_movies)
+
 ##duration, genres, plot_keywords, language, country, budget
 colnames(new_movies)
  
@@ -36,3 +33,9 @@ str(new_movies)
 new_movies$movie_facebook_likes
 new_movies$budget
 
+colnames(new_movies)
+##Colocando a variavel resposta na primeira posicao (coluna)
+new_movies <- new_movies %>%
+  select(imdb_score, everything())
+
+colnames(new_movies)
